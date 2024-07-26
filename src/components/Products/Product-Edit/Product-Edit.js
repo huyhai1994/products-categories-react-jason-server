44790b2716189d277194b21f29aeb3af9588f8c4
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-26 15:34:02 +0700

    add full format for edit form

--- src/components/Products/Product-Edit/Product-Edit.js
+++ src/components/Products/Product-Edit/Product-Edit.js
@@ -11,7 +11,7 @@ const ProductEdit = () => {
         category: Yup.string().required("Category is required"),
         quantity: Yup.number().required("Quantity is required")
     })
-    return (<div>
+    return (<div className='container'>
         id: {id}
         <h1 className='text-center'>Edit user</h1>
         <form className='border p-3 rounded-3'>
@@ -22,15 +22,27 @@ const ProductEdit = () => {
                 />
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" name="email" className="form-control"
-                       id="exampleInputEmail1"
-                       placeholder="name@example.com"/>
+                <label htmlFor="price" className="form-label">Price</label>
+                <input type="number" name="price" className="form-control"
+                       id="price"
+                />
             </div>
             <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" name="password" className="form-control" id="exampleInputPassword1"
+                <label htmlFor="quantity" className="form-label">Date</label>
+                <input type="date" name="date" className="form-control" id="quantity"
                 />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="quantity" className="form-label">Quantity</label>
+                <input type="number" name="quantity" className="form-control" id="quantity"
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="category" className="form-label">Category</label>
+                <select name="category" className="form-control" id="category">
+                    <option value="">Select Category</option>
+                    {/* Add options here */}
+                </select>
             </div>
             <button type="submit" className="btn btn-primary w-100">Submit</button>
         </form>
