f72c6ac846bc46c57b694c7b23bbc77ec26d7cb3
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-26 11:44:08 +0700

    update the state for ProductList

--- src/components/Products/Product-List/ProductList.js
+++ src/components/Products/Product-List/ProductList.js
@@ -2,18 +2,17 @@ import React, {useEffect, useState} from 'react';
 import {Table} from 'react-bootstrap';
 import userService from "../../../services/user.service";
 
-
 const ProductList = () => {
-    const [users, setUsers] = useState([]);
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         userService.getAllProducts().then(response => {
-            setUsers(response.data);
-        })
-    })
+            setProducts(response.data);
+        });
+    }, []); // Added dependency array to useEffect to avoid infinite loop
 
     return (<div className="container">
-        <h4 className="card-title text-center my-5 ">Product List</h4>
+        <h4 className="card-title text-center my-5">Product List</h4>
         <Table striped bordered hover>
             <thead>
             <tr>
@@ -27,18 +26,19 @@ const ProductList = () => {
             </tr>
             </thead>
             <tbody>
-            <tr className="table-row">
-                <td className="text-center"> 1</td>
-                <td className="text-center">Product 1</td>
-                <td className="text-center">99$</td>
-                <td className="text-center">Category1</td>
-                <td className="text-center"> 22/07/2024</td>
-                <td className="text-center"> 13</td>
-                <td className="text-center"> Delete | Edit</td>
-            </tr>
+            {products.map((product, index) => (
+                <tr className={`table-row ${product.active ? "" : "inactive-row"}`} key={product.id}>
+                    <td className="text-center" style={{fontWeight: 'bold'}}>{index + 1}</td>
+                    <td className="text-center">{product.name}</td>
+                    <td className="text-center">{product.price}</td>
+                    <td className="text-center">{product.category.name}</td>
+                    <td className="text-center">{product.date}</td>
+                    <td className="text-center">{product.quantity}</td>
+                    <td className="text-center">Delete | Edit</td>
+                </tr>))}
             </tbody>
         </Table>
-    </div>)
+    </div>);
 };
 
 export default ProductList;
