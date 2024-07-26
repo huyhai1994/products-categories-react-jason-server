97c8934ca7ab11d3bc77a939500030e7486ffd36
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-26 15:25:08 +0700

    add basic bootstrap form to Product-Edit.js

--- src/components/Products/Product-Edit/Product-Edit.js
+++ src/components/Products/Product-Edit/Product-Edit.js
@@ -13,6 +13,27 @@ const ProductEdit = () => {
     })
     return (<div>
         id: {id}
+        <h1 className='text-center'>Edit user</h1>
+        <form className='border p-3 rounded-3'>
+            <div className="mb-3">
+                <label htmlFor="exampleInputName" className="form-label">Name</label>
+                <input type="text" name="name" className="form-control"
+                       id="exampleInputName"
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" name="email" className="form-control"
+                       id="exampleInputEmail1"
+                       placeholder="name@example.com"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" name="password" className="form-control" id="exampleInputPassword1"
+                />
+            </div>
+            <button type="submit" className="btn btn-primary w-100">Submit</button>
+        </form>
     </div>)
 }
 export default ProductEdit
