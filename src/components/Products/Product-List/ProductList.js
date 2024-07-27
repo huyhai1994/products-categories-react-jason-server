7efa7209e8da49b34c8e8e7ec4491b22021d39c4
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-27 10:51:58 +0700

    add paginating to project

--- src/components/Products/Product-List/ProductList.js
+++ src/components/Products/Product-List/ProductList.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {Button, Table} from 'react-bootstrap';
+import {Button, Pagination, Table} from 'react-bootstrap';
 import userService from "../../../services/product.service";
 import {toast} from "react-toastify";
 import {useNavigate} from 'react-router-dom';
 
 const ProductList = () => {
     const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [productsPerPage] = useState(10);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -25,6 +27,12 @@ const ProductList = () => {
         }
     }
 
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
     return (<div className="container">
         <h4 className="card-title text-center my-5">Product List</h4>
         <div className='table-responsive'>
@@ -42,8 +50,8 @@ const ProductList = () => {
                 </tr>
                 </thead>
                 <tbody>
-                {products.map((product, index) => (<tr className="table-row" key={product.id}>
-                    <td className="text-center" style={{fontWeight: 'bold'}}>{index + 1}</td>
+                {currentProducts.map((product, index) => (<tr className="table-row" key={product.id}>
+                    <td className="text-center" style={{fontWeight: 'bold'}}>{indexOfFirstProduct + index + 1}</td>
                     <td className="text-center">{product.name}</td>
                     <td className="text-center">{product.price}</td>
                     <td className="text-center">{product.category.name}</td>
@@ -57,6 +65,13 @@ const ProductList = () => {
                 </tr>))}
                 </tbody>
             </Table>
+            <Pagination className="d-flex justify-content-center ">
+                {Array.from({length: Math.ceil(products.length / productsPerPage)}, (_, index) => (
+                    <Pagination.Item key={index + 1} active={index + 1 === currentPage}
+                                     onClick={() => paginate(index + 1)}>
+                        {index + 1}
+                    </Pagination.Item>))}
+            </Pagination>
         </div>
     </div>);
 };
